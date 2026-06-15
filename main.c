236a9d1a6e34a5398a6cb300efd11b366c9e1b34
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-15 20:51:55 +1200

    Update main.c
    added loops if and else

--- main.c
+++ main.c
@@ -7,5 +7,14 @@ int age;
   printf("enter age:");
   scanf("%d",&age);
   printf("you said %d",age);
+
+  if (age >= 18){
+    printf("you can drink");
+  } else{
+    for (i=0, i < 5,i++){
+      printf("No No No");
+    }
+  }
+  
 return 0;
 }
