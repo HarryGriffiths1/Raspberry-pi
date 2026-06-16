383d58e079d42996bd8ae81bd066b6bb33d9f0b0
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-16 10:51:47 +0000

    button stuff

--- main.c
+++ main.c
@@ -1,25 +1,43 @@
 // hello world
 
 #include <stdio.h>
+#include <wiringPi.h>
 
-void age_check() {
+# define BUTTON 0
+
+
+void age_check(int r_age) {
 int age;
   
   printf("enter age:");
   scanf("%d",&age);
   printf("you said %d",age);
 
-  if (age >= 18){
+  if (age >= r_age){
     printf("you can drink");
   } else{
     for (int i=0; i < 5; i++){
-      printf("No No No");
+      printf("No No No\n");
     }
   }
   
 }
 
 int main(){
-  age_check();
+  wiringPiSetup();
+
+  pinMode(BUTTON, INPUT);
+  pullUpDnControl(BUTTON, PUD_UP);
+
+  while (1)
+    {
+        if (digitalRead(BUTTON) == LOW)
+        {
+            age_check(18);
+        }
+
+        delay(50); // debounce
+    }
+
   return 0;
 }
