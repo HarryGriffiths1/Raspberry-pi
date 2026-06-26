144aa2047877beb9964f8fe85d9d4be657ef5b06
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-26 14:30:37 +0000

    added button stop

--- motor.c
+++ motor.c
@@ -4,6 +4,7 @@
 #define STEP 17
 #define DIR 27
 #define EN 22
+#define BUTTON 27
 
 void main() {
 wiringPiSetupGpio();
@@ -11,16 +12,22 @@ wiringPiSetupGpio();
     pinMode(STEP, OUTPUT);
     pinMode(DIR, OUTPUT);
     pinMode(EN, OUTPUT);
+    pinMode(BUTTON, INPUT);
+    pullUpDnControl(BUTTON, PUD_UP);
 
     digitalWrite(EN, LOW);   // enable
     digitalWrite(DIR, HIGH); // set direction
 
-    while (1){
+    bool pressed = 0
+
+    while (pressed){
 
         digitalWrite(STEP, HIGH);
-        delayMicroseconds(50);
+        delayMicroseconds(50000);
 
         digitalWrite(STEP, LOW);
-        delayMicroseconds(500);
+        delayMicroseconds(50000);
+        
+        pressed = !(digitalRead(BUTTON) == LOW)
     }
 }
