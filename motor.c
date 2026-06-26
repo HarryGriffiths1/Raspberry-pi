8dc9a72bad9b2b3f0b3b71e88922a94163274977
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-26 14:19:44 +0000

    added motor stuff

---
+++ motor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define STEP 17
+#define DIR 27
+#define EN 22
+
+void main() {
+wiringPiSetupGpio();
+
+    pinMode(STEP, OUTPUT);
+    pinMode(DIR, OUTPUT);
+    pinMode(EN, OUTPUT);
+
+    digitalWrite(EN, LOW);   // enable
+    digitalWrite(DIR, HIGH); // set direction
+
+    while 1{
+        
+        digitalWrite(STEP, HIGH);
+        delayMicroseconds(500);
+
+        digitalWrite(STEP, LOW);
+        delayMicroseconds(500);
+    }
+}
