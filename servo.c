1e17273e20b88a6a8a6362e14dcf49549fe4e11a
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-29 11:28:53 +0000

    fixed sintax

--- servo.c
+++ servo.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-# define SERVO 13;
+#define SERVO 13
 
 int main(){
 
     wiringPiSetupGpio();
     pinMode(SERVO, PWM_MS_OUTPUT);
     pwmSetRange(2000); // 1 tick is 0.01 ms so 20ms range
-    pwmSetClock(192); // need 50hz so 19200/ range(2000) / clock(192) = 50hz
+    pwmSetClock(192); // need 50hz so 19200/ range(2000) / clock(192) = 0.05khz
 
-    pwmWrite(SERVO, 100);
+    pwmWrite(SERVO, 100); // set 0
     delayMicroseconds(5000);
-    pwmWrite(SERVO, 150);
+    pwmWrite(SERVO, 150); // set 90
     delayMicroseconds(5000);
-    pwmWrite(SERVO, 200);
+    pwmWrite(SERVO, 200); // set 180
 }
