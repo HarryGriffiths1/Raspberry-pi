85705db6d525eb1880b8d6ee51dfbf7d34a79875
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2026-06-29 11:39:01 +0000

    changed to take input

--- servo.c
+++ servo.c
@@ -12,11 +12,9 @@ int main(){
     pwmSetRange(2000); // 1 tick is 0.01 ms so 20ms range
     pwmSetClock(192); // need 50hz so 19200/ range(2000) / clock(192) = 0.05khz
 
-    pwmWrite(SERVO, 100); // set 0
-    delayMicroseconds(500000);
-    pwmWrite(SERVO, 120); // set 90
-    delayMicroseconds(500000);
-    pwmWrite(SERVO, 150); // set 180
-    delayMicroseconds(500000);
-    pwmWrite(SERVO, 120); // set 90
+    int pulse;
+    printf("go to:");
+    scanf("%d",&pulse);
+
+    pwmWrite(SERVO, pulse); // set to place of "pulse"
 }
